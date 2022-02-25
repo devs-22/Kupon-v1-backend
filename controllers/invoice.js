cb3a62c2bfc7fe9f191fa7e9acdf1f3b1db54945
utibeabasi6 <utibeabasiumanah6@gmail.com>
2022-02-25 16:58:04 +0100

    added files

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -2,14 +2,20 @@ const Invoice = require("../models/invoice")
 
 function getInvoice(req, res) {
     Invoice.find()
-        .then(r => res.json(r))
+        .then(r => res.json({itemCount: r.length, items: r}))
         .catch(err => { throw (err) })
 }
 
 function createInvoice(req, res) {
+    if( req.body.name != ""){
     Invoice.create({ name: req.body.name })
         .then(r => res.json(r))
         .catch(err => { throw (err) })
+    } else{
+        res.status(500).json({
+            error: "Name cannot be blank"
+        })
+    }
 }
 
 module.exports.getInvoice = getInvoice
